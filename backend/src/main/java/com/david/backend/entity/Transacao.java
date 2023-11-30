d69c4fd2179ce09550edc2c121a80cccdce14f6c
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-11-30 08:28:35 -0300

    Processamento de Arquivo CNAB com Spring Batch

---
+++ backend/src/main/java/com/david/backend/entity/Transacao.java
@@ -0,0 +1,41 @@
+package com.david.backend.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public record Transacao(
+        Long id,
+        Integer tipo,
+        Date data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        Time hora,
+        String donoDaLoja,
+        String nomeDaLoja) {
+
+    public Transacao comValor(BigDecimal valor) {
+        return new Transacao(id, tipo, data, valor, cpf, cartao, hora, donoDaLoja, nomeDaLoja);
+    }
+
+    public Transacao comData(String data) throws ParseException {
+        var dataFormatada = new SimpleDateFormat("yyyyMMdd");
+        var novaData = dataFormatada.parse(data);
+
+        return new Transacao(
+                id, tipo, new Date(novaData.getTime()), valor, cpf,
+                cartao, hora, donoDaLoja, nomeDaLoja);
+    }
+
+    public Transacao comHora(String hora) throws ParseException {
+        var horaFormatada = new SimpleDateFormat("HHmmss");
+        var novaHora = horaFormatada.parse(hora);
+
+        return new Transacao(
+                id, tipo, data, valor, cpf,
+                cartao, new Time(novaHora.getTime()), donoDaLoja, nomeDaLoja);
+    }
+}
